fda87520b7634ec01d923d8f76b346c5cf59fb6b
Daniel Leech <dan.t.leech@gmail.com>
2020-05-10 09:46:45 +0100

    Member references

--- lib/Model/Query/MemberQuery.php
+++ lib/Model/Query/MemberQuery.php
@@ -2,8 +2,11 @@
 
 namespace Phpactor\Indexer\Model\Query;
 
+use Generator;
 use Phpactor\Indexer\Model\Index;
 use Phpactor\Indexer\Model\IndexQuery;
+use Phpactor\Indexer\Model\Record\FileRecord;
+use Phpactor\TextDocument\Location;
 use Phpactor\Indexer\Model\Record\MemberRecord;
 
 class MemberQuery implements IndexQuery
@@ -30,5 +33,23 @@ class MemberQuery implements IndexQuery
     public function getByTypeAndName(string $type, string $name): ?MemberRecord
     {
         return $this->get($type . '#' . $name);
+    }
+
+    /**
+     * @return Generator<Location>
+     */
+    public function referencesTo(string $type, string $memberName): Generator
+    {
+        $record = $this->getByTypeAndName($type, $memberName);
+        assert($record instanceof MemberRecord);
+
+        foreach ($record->references() as $fileReference) {
+            $fileRecord = $this->index->get(FileRecord::fromPath($fileReference));
+            assert($fileRecord instanceof FileRecord);
+
+            foreach ($fileRecord->references()->to($record) as $classReference) {
+                yield Location::fromPathAndOffset($fileRecord->filePath(), $classReference->offset());
+            }
+        }
     }
 }
